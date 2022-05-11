8233700988b459ff725d7e026725b1c0d4cb20fd
Eduardo Turcios <eddieturcios@outlook.com>
2022-05-11 12:48:20 -0400

    updated routes

--- client/Routes.js
+++ client/Routes.js
@@ -1,33 +1,27 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
-import { withRouter, Route, Switch, Redirect } from 'react-router-dom';
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { Login, Signup } from './components/AuthForm';
 import Home from './components/Home';
-import { me } from './store';
 import Blockchain from './components/Blockchain';
-/**
- * COMPONENT
- */
-export default class Routes extends Component {
-  render() {
-    const isLoggedIn = false;
+import { useAuth } from './context/AuthContext';
 
-    return (
-      <div>
-        {isLoggedIn ? (
-          <Switch>
-            <Route path="/blockchain" component={Blockchain} />
-            <Route path="/home" component={Home} />
-            <Redirect to="/home" />
-          </Switch>
-        ) : (
-          <Switch>
-            <Route path="/" exact component={Login} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-          </Switch>
-        )}
-      </div>
-    );
-  }
+export default function Routes() {
+  const { currentUser } = useAuth();
+  return (
+    <div>
+      {currentUser ? (
+        <Switch>
+          <Route path="/blockchain" component={Blockchain} />
+          <Route path="/home" component={Home} />
+          <Redirect to="/home" />
+        </Switch>
+      ) : (
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+        </Switch>
+      )}
+    </div>
+  );
 }
