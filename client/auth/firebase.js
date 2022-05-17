73a1be4301ed6fcee88cf3ed03aac3e0739c5a7c
Brian Clark <briansclark1@gmail.com>
2022-05-17 15:45:53 -0400

    Merging to main

--- client/auth/firebase.js
+++ client/auth/firebase.js
@@ -10,7 +10,7 @@ const app = firebase.initializeApp({
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
-firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
 
 export const auth = app.auth();
 
