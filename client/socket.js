af15e75ee3d02dd50ee238428ad4c1d416e7f24d
Eduardo Turcios <eddieturcios@outlook.com>
2022-05-26 15:48:44 -0400

    Updated port number

--- client/socket.js
+++ client/socket.js
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client';
+const PORT = process.env.PORT || 8080;
 
-const SOCKET_URL = 'https://localhost:8080';
+const SOCKET_URL = `https://localhost:${PORT}`;
 const option = {
   autoConnect: false,
   withCredentials: true,
