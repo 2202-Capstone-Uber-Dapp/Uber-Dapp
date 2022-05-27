d65e611b6e6f91231eb2d633b97d6be66e13bd67
Eduardo Turcios <eddieturcios@outlook.com>
2022-05-26 22:16:12 -0400

    Fixed Dashboard

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -41,16 +41,12 @@ import { Link as RouterLink } from 'react-router-dom';
 import { useSocket } from '../context/SocketContext';
 const DriverItems = [
   { name: 'Home', icon: FiHome, path: '/' },
-  { name: 'Trending', icon: FiTrendingUp, path: '/trending' },
-  { name: 'Dashboard', icon: FaUber, path: '/dashboard' },
   { name: 'Wallet', icon: FaWallet, path: '/wallet' },
   { name: 'Settings', icon: FiSettings, path: '/setting' },
 ];
 
 const RiderItems = [
   { name: 'Home', icon: FiHome, path: '/' },
-  { name: 'Trending', icon: FiTrendingUp, path: '/trending' },
-  { name: 'Dashboard', icon: FaUber, path: '/dashboard' },
   { name: 'Wallet', icon: FaWallet, path: '/wallet' },
   { name: 'Settings', icon: FiSettings, path: '/setting' },
 ];
@@ -224,47 +220,22 @@ const MobileNav = ({ onOpen, ...rest }) => {
       </Text>
 
       <HStack spacing={{ base: '0', md: '6' }}>
-        <IconButton
-          size="lg"
-          variant="ghost"
-          aria-label="open menu"
-          icon={<FiBell />}
-        />
         <Flex alignItems={'center'}>
-          <Menu>
-            <MenuButton
-              py={2}
-              transition="all 0.3s"
-              _focus={{ boxShadow: 'none' }}
+          <HStack>
+            <Avatar size={'sm'} src={user.profileImage} />
+            <VStack
+              display={{ base: 'none', md: 'flex' }}
+              alignItems="flex-start"
+              spacing="1px"
+              ml="2"
             >
-              <HStack>
-                <Avatar size={'sm'} src={user.profileImage} />
-                <VStack
-                  display={{ base: 'none', md: 'flex' }}
-                  alignItems="flex-start"
-                  spacing="1px"
-                  ml="2"
-                >
-                  <Text fontSize="sm">{user.username}</Text>
-                  <Text fontSize="xs" color="gray.600">
-                    {user.role}
-                  </Text>
-                </VStack>
-                <Box display={{ base: 'none', md: 'flex' }}>
-                  <FiChevronDown />
-                </Box>
-              </HStack>
-            </MenuButton>
-            <MenuList
-              bg={useColorModeValue('white', 'gray.900')}
-              borderColor={useColorModeValue('gray.200', 'gray.700')}
-            >
-              <MenuItem>Profile</MenuItem>
-              <MenuItem>Billing</MenuItem>
-              <MenuDivider />
-              <MenuItem>Sign out</MenuItem>
-            </MenuList>
-          </Menu>
+              <Text fontSize="sm">{user.username}</Text>
+              <Text fontSize="xs" color="gray.600">
+                {user.role}
+              </Text>
+            </VStack>
+            <Box display={{ base: 'none', md: 'flex' }}></Box>
+          </HStack>
         </Flex>
       </HStack>
     </Flex>
